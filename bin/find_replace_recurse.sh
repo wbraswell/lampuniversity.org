fc9725d8acb85d635e712e581c8e9d9d81711a36
Will Braswell <william.braswell@autoparallel.com>
2014-12-08 04:19:44 -0600

    Find Replace Recursive Script, Add Usage Examples

--- bin/find_replace_recurse.sh
+++ bin/find_replace_recurse.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Copyright © 2014, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.20.0.
 
+echo EXAMPLES:
+echo replace all occurrences of 'FOO' with 'BAR'
+echo $ find_replace_recurse.sh 'FOO' 'BAR' ./
+echo replace all occurrences of '[ ANYTHING GOES HERE 123 #@$ ]' with '< ANYTHING GOES HERE 123 #@$ >'
+echo $ find_replace_recurse.sh '\[(.*)\]' '\<\1\>' ./
+echo
+
 echo "ABOUT TO RECURSIVELY FIND AND REPLACE"
-echo "directory: $1"
-echo "find text: $2"
-echo "replace text: $3"
+echo "find text: $1"
+echo "replace text: $2"
+echo "directory: $3"
 
 read -p "Are you sure? " -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    sed -ri -e "s/$2/$3/g" $(grep -Elr --binary-files=without-match "$2" "$1")
+    sed -ri -e "s/$1/$2/g" $(grep -Elr --binary-files=without-match "$1" "$3")
 
     # wrongly edits binary files, will corrupt .git/index files, database files, etc
-#    find $1 -type f -readable -writable -exec sed -i "s/$2/$3/g" {} \;
+#    find $3 -type f -readable -writable -exec sed -i "s/$1/$2/g" {} \;
 fi
