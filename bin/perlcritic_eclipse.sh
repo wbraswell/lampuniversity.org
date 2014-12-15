e27ad481c2ff62dae94f6e6686d8f4a0ecaf0be8
Will Braswell <william.braswell@autoparallel.com>
2014-12-15 03:11:38 -0600

    Perl Critic for Eclipse, Remove Hard-Coded Username

--- bin/perlcritic_eclipse.sh
+++ bin/perlcritic_eclipse.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Copyright © 2014, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.20.0.
+USERNAME=`whoami`
 
-#cd /home/wbraswell/austin_perl_mongers/rperl/rperl-latest
+#cd /home/$USERNAME/austin_perl_mongers/rperl/rperl-latest
 #pwd
 #echo "1 " $1 > /tmp/perlcritic_args.out
 #echo "2 " $2 >> /tmp/perlcritic_args.out
@@ -9,6 +10,6 @@
 #echo "4 " $4 >> /tmp/perlcritic_args.out
 #echo "5 " $5 >> /tmp/perlcritic_args.out
 #echo "6 " $6 >> /tmp/perlcritic_args.out
-#/home/wbraswell/perl5/bin/perlcritic $1 --verbose "$3" > /tmp/perlcritic.out
-#/home/wbraswell/perl5/bin/perlcritic $1 --verbose "$3"
-/home/wbraswell/perl5/bin/perlcritic $1 $2 "$3"
+#/home/$USERNAME/perl5/bin/perlcritic $1 --verbose "$3" > /tmp/perlcritic.out
+#/home/$USERNAME/perl5/bin/perlcritic $1 --verbose "$3"
+/home/$USERNAME/perl5/bin/perlcritic $1 $2 "$3"
