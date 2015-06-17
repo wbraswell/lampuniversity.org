82904056c40532276d4912c462b082492b93f90e
Will Braswell <william.braswell@autoparallel.com>
2015-06-17 00:14:02 -0500

    Find Replace Recursive Script, Update Character Escape Info, Part 5

--- bin/find_replace_recurse.sh
+++ bin/find_replace_recurse.sh
@@ -6,9 +6,9 @@ echo "replace all occurrences of 'FOO' with 'BAR'"
 echo "$ find_replace_recurse.sh 'FOO' 'BAR' ./"
 echo "replace all occurrences of '[ ANYTHING GOES HERE 123 #@$ ]' with '< ANYTHING GOES HERE 123 #@$ >'"
 echo "$ find_replace_recurse.sh '\[(.*)\]' '<\1>' ./"
-echo 'BACKSLASH-ESCAPED CHARACTERS, find text: $ & ( ) [ ] { } | / \'
+echo 'BACKSLASH-ESCAPED CHARACTERS, find text: $ @ % & ( ) [ ] { } | / \'
 echo 'NON-BACKSLASH-ESCAPED CHARACTERS, find text: < > #'
-echo 'BACKSLASH-ESCAPED CHARACTERS, replace text: / \ # &'
+echo 'BACKSLASH-ESCAPED CHARACTERS, replace text: @ % & / \ #'
 echo 'BACKSLASH-ESCAPED CHARACTERS, replace text matches: 1 2 3 ...'
 echo 'NON-BACKSLASH-ESCAPED CHARACTERS, replace text: $ ( ) [ ] { } < >'
 echo
