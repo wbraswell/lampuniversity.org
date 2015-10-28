6ff54ea15a44a4a5a3b1583a532ac6845b22928f
Will Braswell <william.braswell@autoparallel.com>
2015-10-27 23:27:57 -0500

    Utility Scripts, NTP Ubuntu Updater, Initial Commit

---
+++ bin/ntpdate_ubuntu_update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Copyright © 2015, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.20.0.
+
+sudo sh -c "service ntp stop; ntpdate ntp.ubuntu.com; service ntp start"
