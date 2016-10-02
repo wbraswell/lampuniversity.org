6da1616c00ee237a2899add3e38ad9785e456778
Will Braswell <william.braswell@autoparallel.com>
2016-10-02 16:10:49 -0500

    LAMP Install Notes, Reorganize

---
+++ bin/LAMP_installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright © 2016, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.24.0.
+
+echo [[[ LAMP Installer Script ]]]
+echo
+echo Choose one or press <ENTER> for 0...
+echo
+echo 
