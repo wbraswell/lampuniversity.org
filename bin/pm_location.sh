14942196cf14186248eaa271c0937f8d4286d794
Will Braswell <william.braswell@autoparallel.com>
2016-11-22 05:13:48 -0600

    Perl Module Locator, Initial Commit

---
+++ bin/pm_location.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright © 2016, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.22.0.
+
+# pre-munged example
+# perl -e 'use ExtUtils::MakeMaker; $s = q{ExtUtils::MakeMaker}; $s =~ s/::/\//g; $s .= q{.pm}; print $INC{$s}, "\n";'
+
+perl -e "use ${1}; \$s = q{${1}}; \$s =~ s/::/\//g; \$s .= q{.pm}; print \$INC{\$s}, qq{\n};"
