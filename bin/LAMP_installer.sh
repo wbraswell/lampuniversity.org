93f1a39fb781f4cf7119460272ac897a2aa28df7
Will Braswell <william.braswell@autoparallel.com>
2016-11-25 01:49:53 -0600

    LAMP Installer, Section 6, Add htop

--- bin/LAMP_installer.sh
+++ bin/LAMP_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright © 2014, 2015, 2016, William N. Braswell, Jr.. All Rights Reserved. This work is Free \& Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.24.0.
 # LAMP Installer Script
-VERSION='0.096_000'
+VERSION='0.098_000'
 
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
@@ -10,7 +10,7 @@ VERSION='0.096_000'
 # PRE-INSTALL: download the latest version of this file and make it executable
 # wget https://raw.githubusercontent.com/wbraswell/lampuniversity.org/master/bin/LAMP_installer.sh; chmod a+x ./LAMP_installer.sh
 # OR
-# wget http://tinyurl.com/lampinstaller; chmod a+x ./LAMP_installer.sh
+# wget http://tinyurl.com/lampinstaller -O LAMP_installer.sh; chmod a+x ./LAMP_installer.sh
 
 # enable extended pattern matching in case statements
 shopt -s extglob
@@ -479,9 +479,9 @@ if [ $MENU_CHOICE -le 6 ]; then
         echo '[ Check Install, Confirm No Errors ]'
         S apt-get update
         S apt-get -f install
-        echo '[ General Tools: g++ ssh perl perl-doc vim git linuxlogo lynx screen ]'
+        echo '[ General Tools: g++ make ssh perl perl-doc vim git htop linuxlogo lynx screen ]'
         echo '[ LAMP University Tools Requirements: zip unzip ]'
-        S apt-get install g++ ssh perl perl-doc vim git linuxlogo lynx screen zip unzip
+        S apt-get install g++ make ssh perl perl-doc vim git htop linuxlogo lynx screen zip unzip
         echo '[ Check Install, Confirm No Errors ]'
         S apt-get -f install
     elif [ $MACHINE_CHOICE -eq 1 ]; then
