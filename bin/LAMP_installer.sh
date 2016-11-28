e426b63837103b574cdcc4b27c92839dd2cbe873
Will Braswell <william.braswell@autoparallel.com>
2016-11-28 01:33:21 -0600

    LAMP Installer, Section 31, Add Editor Prompt

--- bin/LAMP_installer.sh
+++ bin/LAMP_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright © 2014, 2015, 2016, William N. Braswell, Jr.. All Rights Reserved. This work is Free \& Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.24.0.
 # LAMP Installer Script
-VERSION='0.102_000'
+VERSION='0.103_000'
 
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
@@ -1445,6 +1445,8 @@ if [ $MENU_CHOICE -le 31 ]; then
     echo '31. [[[ UBUNTU LINUX, INSTALL APACHE & MOD_PERL ]]]'
     echo
     if [ $MACHINE_CHOICE -eq 0 ]; then
+        D $EDITOR 'preferred text editor' 'vi'
+        EDITOR=$USER_INPUT
         D $USERNAME "new machine's username" `whoami`
         USERNAME=$USER_INPUT
 
