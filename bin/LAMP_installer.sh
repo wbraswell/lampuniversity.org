0daabd3781e4425a2aada1426d85cc1c6740009c
Will Braswell <william.braswell@autoparallel.com>
2017-04-12 18:10:58 -0500

    Pluto, Add New Official Github Repo

--- bin/LAMP_installer.sh
+++ bin/LAMP_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright © 2014, 2015, 2016, 2017, William N. Braswell, Jr.. All Rights Reserved. This work is Free \& Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.24.0.
 # LAMP Installer Script
-VERSION='0.120_000'
+VERSION='0.121_000'
 
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
@@ -1089,7 +1089,8 @@ if [ $MENU_CHOICE -le 25 ]; then
         B astyle -V
 
         echo '[ ANY OPTION: Install RPerl Dependency Pluto PolyCC, Download ]'
-        B 'wget https://github.com/wbraswell/pluto-mirror/raw/master/backup/pluto-0.11.4.tar.gz; tar -xzvf pluto-0.11.4.tar.gz'
+        # B 'wget https://github.com/wbraswell/pluto-mirror/raw/master/backup/pluto-0.11.4.tar.gz; tar -xzvf pluto-0.11.4.tar.gz'  # prefer official repo below
+        B 'wget https://github.com/bondhugula/pluto/files/737550/pluto-0.11.4.tar.gz; tar -xzvf pluto-0.11.4.tar.gz'
         echo '[ ANY OPTION: Install RPerl Dependency Pluto PolyCC, Build ]'
         B 'cd pluto-0.11.4; ./configure; make; make test'
         echo '[ ANY OPTION: Install RPerl Dependency Pluto PolyCC, Install ]'
