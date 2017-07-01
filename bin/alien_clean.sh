f794253dd04b48660672e23bd6dcdb95bd621cc2
Will Braswell <william.braswell@autoparallel.com>
2017-06-30 20:20:02 -0500

    Alien Clean, Initial Commit

---
+++ bin/alien_clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright © 2017, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.20.0.
+
+# NEED TEST: is there any reason to prefer this line over the ones below?
+# find . -name "_alien" -type d -exec rm -rf {} +
+
+find . -type d -name _alien
+rm -Rf `find . -type d -name _alien`
