2e6c6a0298535410d61112fb926fa0a1728ac23f
Will Braswell <william.braswell@autoparallel.com>
2017-07-11 21:21:08 -0500

    Perl Module Location, Fix Missing my Bug

--- bin/pm_location.sh
+++ bin/pm_location.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Copyright © 2016, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.22.0.
+# Copyright © 2016 2017, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.22.0.
 
 # pre-munged example
-# perl -e 'use ExtUtils::MakeMaker; $s = q{ExtUtils::MakeMaker}; $s =~ s/::/\//g; $s .= q{.pm}; print $INC{$s}, "\n";'
+# perl -e 'use ExtUtils::MakeMaker; my $s = q{ExtUtils::MakeMaker}; $s =~ s/::/\//g; $s .= q{.pm}; print $INC{$s}, "\n";'
 
-perl -e "use ${1}; \$s = q{${1}}; \$s =~ s/::/\//g; \$s .= q{.pm}; print \$INC{\$s}, qq{\n};"
+perl -e "use ${1}; my \$s = q{${1}}; \$s =~ s/::/\//g; \$s .= q{.pm}; print \$INC{\$s}, qq{\n};"
