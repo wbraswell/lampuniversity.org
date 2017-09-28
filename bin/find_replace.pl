3523a2eba3d1c12ac55699445944db707f64815e
Will Braswell <william.braswell@autoparallel.com>
2017-09-28 16:39:53 -0500

    Find Replace Recurse, Clarify Space Character Status

---
+++ bin/find_replace.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+my $find = shift @ARGV;
+my $replace = shift @ARGV;
+
+print 'have $find = ', "\n", $find, "\n\n";
+print 'have $replace = ', "\n", $replace, "\n\n";
+
+# BEGIN RAW CODE
+
+foreach $arg (@ARGV) { 
+    next if not -f $arg;
+
+    print "opening $arg... ";
+    (open my $FH, "<", $arg) or die $!;
+
+    my $s = q{};
+    while (<$FH>) { $s .= $_; }
+    print "have \$s = $s\n\n";
+
+    print "replacing";
+    while ($s =~ m/$find/gxms) {
+        $s =~ s/$find/$replace/gxms;
+        print ".";
+    }
+    print "\n";
+
+    (close $FH) or die $!;
+
+    (open $FH, ">", $arg) or die $!;
+    print {$FH} $s;
+    (close $FH) or die $!;
+}
+
+# END RAW CODE
--- bin/find_replace_recurse.sh
+++ bin/find_replace_recurse.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 # Copyright © 2014, 2015, 2016, 2017, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.20.0.
 
-VERSION=0.100_000
+VERSION=0.121_000
 
-echo "This program uses 'grep -Pzlr' for searching and 'perl -e "... s/FIND/REPLACE/gxms ..."' for replacement."
-echo "Please see the Perl documentation for more info:  https://perldoc.perl.org/perlre.html"
-echo "[[[ USAGE EXAMPLES ]]]"
-echo "1. Replace All Occurrences Of 'FOO' With 'BAR'"
-echo "$ find_replace_recurse.sh 'FOO' 'BAR' ./"
-echo "2. Replace All Occurrences Of '[ ANYTHING GOES HERE 123 #@$ ]' With '< ANYTHING GOES HERE 123 #@$ >'"
-echo "$ find_replace_recurse.sh '\[(.*)\]' '<\$1>' ./"
-echo 'NOTE: all regular expressions are greedy by default, beware of using (.*) when one line contains multiple matches'
-echo "3. Replace All Occurrences Of \"FOO\" With \"'FOO'\" (In Other Words, Wrap FOO In Single Quotes)"
-echo "$ find_replace_recurse.sh 'FOO' \"'FOO'\" ./"
-echo "[[[ CHARACTER ESCAPE RULES ]]]"
-echo "   REFERENCE OF ALL CHARACTERS,  any text,               any quotes: \" ' \` ~ ! @ # $ % ^ & * ( ) [ ] { } < > | \\ / - _ + = : ; ? , ."
-echo "           UNUSABLE CHARACTERS, find text,            single quotes:   '"
-echo "             NORMAL CHARACTERS, find text,            single quotes: \"   \` ~ ! @     %   &         ] { } < >       - _   = : ;   ,"
-echo "  BACKSLASH-ESCAPED CHARACTERS, find text,            single quotes:             # $   ^   * ( ) [           | \\ /     +       ?   ."
-echo "             NORMAL CHARACTERS, find text,            double quotes:   '   ~ ! @     %   &         ] { } < >       - _   = : ;   ,"
-echo "  BACKSLASH-ESCAPED CHARACTERS, find text,            double quotes: \"   \`       #     ^   * ( ) [           |   /     +       ?   ."
-echo "2-BACKSLASH-ESCAPED CHARACTERS, find text,            double quotes:               $"
-echo "3-BACKSLASH-ESCAPED CHARACTERS, find text,            double quotes:                                           \\"
-echo "           UNUSABLE CHARACTERS, replace text,         single quotes:   '"
-echo "             NORMAL CHARACTERS, replace text,         single quotes: \"   \` ~ ! @ #   % ^ & * ( ) [ ] { } < > |     - _ + = : ; ? , ."
-echo "  BACKSLASH-ESCAPED CHARACTERS, replace text,         single quotes:               $                           \\ /"
-echo "             NORMAL CHARACTERS, replace text,         double quotes:   '   ~ ! @ #   % ^ & * ( ) [ ] { } < > |     - _ + = : ; ? , ."
-echo "  BACKSLASH-ESCAPED CHARACTERS, replace text,         double quotes: \"   \`                                       /"
-echo "2-BACKSLASH-ESCAPED CHARACTERS, replace text,         double quotes:               $"
-echo "3-BACKSLASH-ESCAPED CHARACTERS, replace text,         double quotes:                                           \\"
-echo
+if [[ $4 =~ ^NO_HELP$ ]] || [[ $5 =~ ^NO_HELP$ ]]
+then
+    echo
+else
+    echo "This program uses 'grep -Pzlr' for searching and 'perl -e "... s/FIND/REPLACE/gxms ..."' for replacement."
+    echo "Please see the Perl documentation for more info:  https://perldoc.perl.org/perlre.html"
+    echo "[[[ USAGE EXAMPLES ]]]"
+    echo "1. Replace All Occurrences Of 'FOO' With 'BAR'"
+    echo "$ find_replace_recurse.sh 'FOO' 'BAR' ./"
+    echo "2. Replace All Occurrences Of '[ ANYTHING GOES HERE 123 #@$ ]' With '< ANYTHING GOES HERE 123 #@$ >'"
+    echo "$ find_replace_recurse.sh '\[(.*)\]' '<\$1>' ./"
+    echo 'NOTE: all regular expressions are greedy by default, beware of using (.*) when one line contains multiple matches'
+    echo "3. Replace All Occurrences Of \"FOO\" With \"'FOO'\" (In Other Words, Wrap FOO In Single Quotes)"
+    echo "$ find_replace_recurse.sh 'FOO' \"'FOO'\" ./"
+    echo "[[[ CHARACTER ESCAPE RULES ]]]"
+    echo "   REFERENCE OF ALL CHARACTERS,  any text,               any quotes: SPACE \" ' \` ~ ! @ # $ % ^ & * ( ) [ ] { } < > | \\ / - _ + = : ; ? , ."
+    echo "           UNUSABLE CHARACTERS, find text,            single quotes:         '"
+    echo "             NORMAL CHARACTERS, find text,            single quotes:       \"   \` ~ ! @     %   &         ] { } < >       - _   = : ;   ,"
+    echo "  BACKSLASH-ESCAPED CHARACTERS, find text,            single quotes: SPACE             # $   ^   * ( ) [           | \\ /     +       ?   ."
+    echo "             NORMAL CHARACTERS, find text,            double quotes:         '   ~ ! @     %   &         ] { } < >       - _   = : ;   ,"
+    echo "  BACKSLASH-ESCAPED CHARACTERS, find text,            double quotes: SPACE \"   \`       #     ^   * ( ) [           |   /     +       ?   ."
+    echo "2-BACKSLASH-ESCAPED CHARACTERS, find text,            double quotes:                     $"
+    echo "3-BACKSLASH-ESCAPED CHARACTERS, find text,            double quotes:                                                 \\"
+    echo "           UNUSABLE CHARACTERS, replace text,         single quotes:         '"
+    echo "             NORMAL CHARACTERS, replace text,         single quotes: SPACE \"   \` ~ ! @ #   % ^ & * ( ) [ ] { } < > |     - _ + = : ; ? , ."
+    echo "  BACKSLASH-ESCAPED CHARACTERS, replace text,         single quotes:                     $                           \\ /"
+    echo "             NORMAL CHARACTERS, replace text,         double quotes: SPACE   '   ~ ! @ #   % ^ & * ( ) [ ] { } < > |     - _ + = : ; ? , ."
+    echo "  BACKSLASH-ESCAPED CHARACTERS, replace text,         double quotes:       \"   \`                                       /"
+    echo "2-BACKSLASH-ESCAPED CHARACTERS, replace text,         double quotes:                     $"
+    echo "3-BACKSLASH-ESCAPED CHARACTERS, replace text,         double quotes:                                                 \\"
+    echo
+fi
 
 echo "ABOUT TO RECURSIVELY FIND AND REPLACE"
 echo "find text:    '$1'"
@@ -55,19 +60,22 @@ if [ "$PASS" -eq 1 ]
 then
     echo 'Recursively finding and replacing...'
 
+# wrongly edits binary files, will corrupt .git/index files, database files, etc
+#    find $3 -type f -readable -writable -exec sed -i "s/$1/$2/g" {} \;
+
 # does not support multi-line search or replace
 #    sed -ri -e "s/$1/$2/g" $(grep -Elr --binary-files=without-match "$1" "$3")
 
 # does not support multi-line replace
 #    perl -pi -e "s/$1/$2/gxms" $(grep -Pzlr --binary-files=without-match "$1" "$3")
 
-# does support multi-line search & replace
-    perl -e "foreach \$arg (@ARGV) { next if not -f \$arg; print \"opening \$arg...\\n\"; (open my \$FH, \"<\", \$arg) or die \$!; my \$s = q{}; while (<\$FH>) { \$s .= \$_; } \$s =~ s/$1/$2/gxms; (close \$FH) or die \$!; (open \$FH, \">\", \$arg) or die \$!; print {\$FH} \$s; (close \$FH) or die \$!; }" $(grep -Pzlr --binary-files=without-match "$1" "$3" | sort)
-# original Perl one-liner code, without additional backslashes:
-#perl -e 'foreach $arg (@ARGV) { next if not -f $arg; print "opening $arg...\n"; (open my $FH, "<", $arg) or die $!; my $s = q{}; while (<$FH>) { $s .= $_; } $s =~ s/FOO/BAR/gxms; (close $FH) or die $!; (open $FH, ">", $arg) or die $!; print {$FH} $s; (close $FH) or die $!; }' ./*
+# does not support multiple replacements per file
+#    perl -e "foreach \$arg (@ARGV) { next if not -f \$arg; print \"opening \$arg...\\n\"; (open my \$FH, \"<\", \$arg) or die \$!; my \$s = q{}; while (<\$FH>) { \$s .= \$_; } \$s =~ s/$1/$2/gxms; (close \$FH) or die \$!; (open \$FH, \">\", \$arg) or die \$!; print {\$FH} \$s; (close \$FH) or die \$!; }" $(grep -Pzlr --binary-files=without-match "$1" "$3" | sort)
 
-    # wrongly edits binary files, will corrupt .git/index files, database files, etc
-#    find $3 -type f -readable -writable -exec sed -i "s/$1/$2/g" {} \;
+# does support multi-line search & replace, multiple replacements per file
+    perl -e "foreach \$arg (@ARGV) { next if not -f \$arg; print \"    opening \$arg... \"; (open my \$FH, \"<\", \$arg) or die \$!; my \$s = q{}; while (<\$FH>) { \$s .= \$_; } print \"replacing\"; while (\$s =~ s/$1/$2/gxms) { print \".\"; } print \"\\n\"; (close \$FH) or die \$!; (open \$FH, \">\", \$arg) or die \$!; print {\$FH} \$s; (close \$FH) or die \$!; }" $(grep -Pzlr --binary-files=without-match "$1" "$3" | sort)
+# original Perl one-liner code, without additional backslashes:
+#perl -e 'foreach $arg (@ARGV) { next if not -f $arg; print "    opening $arg... "; (open my $FH, "<", $arg) or die $!; my $s = q{}; while (<$FH>) { $s .= $_; } print "replacing"; while ($s =~ s/$1/$2/gxms) { print "."; } print "\n"; (close $FH) or die $!; (open $FH, ">", $arg) or die $!; print {$FH} $s; (close $FH) or die $!; }' ./*
 
     echo 'DONE!'
 fi
