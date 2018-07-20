5fc8cd7a6a4ad5a5673ca4aa8b0906e771745dec
Will Braswell <william.braswell@autoparallel.com>
2018-07-19 23:02:43 -0500

    LAMP Installer: ShinyCMS, FastCGI Automatic Startup, SystemD Support, Part 3

--- bin/LAMP_installer.sh
+++ bin/LAMP_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright © 2014, 2015, 2016, 2017, 2018, William N. Braswell, Jr.. All Rights Reserved. This work is Free \& Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.24.0.
 # LAMP Installer Script
-VERSION='0.233_000'
+VERSION='0.234_000'
 
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
 # IMPORTANT DEV NOTE: do not edit anything in this file without making the exact same changes to rperl_installer.sh!!!
@@ -2860,11 +2860,10 @@ END_HEREDOC
 #        S ln -s /home/$USERNAME/public_html/$DOMAIN_NAME-latest/modified/fastcgi_$DOMAIN_NAME.service /etc/systemd/system  # causes "Failed to execute operation: Too many levels of symbolic links"
         S systemctl enable /home/$USERNAME/public_html/$DOMAIN_NAME-latest/modified/fastcgi_$DOMAIN_NAME.service
         S systemctl daemon-reload
-        echo '[ If SystemD Errors Occured On Previous Step, Analyze; If No Errors, Safe To Skip Analyze Command ]'
         S systemd-analyze verify /etc/systemd/system/multi-user.target.wants/fastcgi_$DOMAIN_NAME.service
         S systemctl start fastcgi_$DOMAIN_NAME.service
         S systemctl status fastcgi_$DOMAIN_NAME.service
-        B systemctl list-units | grep fastcgi
+        B "systemctl list-units | grep fastcgi"
 
         # OR
 
