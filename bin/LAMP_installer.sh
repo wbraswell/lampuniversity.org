7e8fb8e52fe7e78c8a2a293edaedafd31dc8692a
Will Braswell <william.braswell@autoparallel.com>
2018-12-18 17:44:22 -0600

    LAMP Installer, RPerl Install, Temporarily Add Missing Alien::GMP Dependency File::Which, Part 4

--- bin/LAMP_installer.sh
+++ bin/LAMP_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright © 2014, 2015, 2016, 2017, 2018, William N. Braswell, Jr.. All Rights Reserved. This work is Free \& Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.24.0.
 # LAMP Installer Script
-VERSION='0.350_000'
+VERSION='0.360_000'
 
 
 # START HERE: sync w/ rperl_installer.sh
@@ -2284,8 +2284,8 @@ if [ $SECTION_CHOICE -le 24 ]; then
             echo
             echo '[ Install Problematic RPerl Dependency IO::Socket::SSL, Skip Tests ]'
             B cpanm -v --notest IO::Socket::SSL
-            echo '[ Install Missing Alien::GMP Dependency File::Which ]'
-            B cpanm -v --notest File::Which FFI::CheckLib Alien::GMP
+            echo '[ Install Missing Alien::GMP Dependencies ]'
+            B cpanm -v --notest File::Which FFI::CheckLib Path::Tiny Alien::GMP
             echo '[ Install RPerl ]'
             B cpanm -v --notest RPerl
 
