a7e09e7fa58ab33e975b14d4d81b6ae4b92e5ad1
William N. Braswell, Jr <william.braswell@autoparallel.com>
2021-01-02 18:43:02 -0600

    Update Groups Utility, Initial Commit

---
+++ bin/update_groups__SOURCE_ME.sh
@@ -0,0 +1,4 @@
+# Copyright © 2021, William N. Braswell, Jr.. All Rights Reserved. This work is Free & Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.
+
+# update user's groups, without logging out and logging back in again
+exec su -l $USER
