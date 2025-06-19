5d1408e3d4d0fda5100da1f0808f8a79ad6ce1cf
Will Braswell <william.braswell@autoparallel.com>
2025-06-19 01:30:27 -0500

    Synaptics Driver Touchpad Scripts

---
+++ bin/touchpad_enable_synaptics.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Copyright © 2025, William N. Braswell, Jr.. All Rights Reserved. This work is Free \& Open Source; you can redistribute it and/or modify it under the same terms as Perl 5.
+# Enable Synaptics Driver Touchpad
+
+synclient TouchpadOff=0
